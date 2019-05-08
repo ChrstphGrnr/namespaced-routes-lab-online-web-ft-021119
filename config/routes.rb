e96f9c24a5419230d301ebba3a3e66adf0a8ef3e
ChrstphGrnr <ChristophGruener@me.com>
2019-05-08 15:51:55 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do 
+    resources :preferences, only: [:index]
+  end
+
+  resources :songs, :artists
+
+  resources :artists do 
+    resources :songs, only: [:show, :index]
+  end
+
 end
